40f47883ba593d1a57e200f21209c2964072b0e5
SEOJINWOONG <tjwlsdnd12@gmail.com>
2024-11-08 15:50:13 +0900

    FIRST COMMIT

---
+++ src/views/AboutView.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>AbouteView</div>
+	<p>{{ $route.path }}</p>
+	<button class="btn btn-primary" @click="$router.push('/')">
+		Home으로 이동하기
+	</button>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+console.log('route.path', route.path);
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<div>HoviewView</div>
+		<p>{{ $route.path }}</p>
+		<p>{{ $route.name }}</p>
+		<button class="btn btn-primary" @click="goAboutPage">
+			About으로 이동하기
+		</button>
+	</div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const goAboutPage = () => {
+	router.push('/about');
+};
+</script>
+
+<style lang="scss" scoped></style>
