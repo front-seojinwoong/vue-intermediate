8a2e55973fc210fd4740b95b28f677733a5432eb
SEOJINWOONG <tjwlsdnd12@gmail.com>
2025-02-11 17:37:33 +0900

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>aboutview</div>
+  <button class="btn btn-primary" @click="$router.push('/')">
+    홈으로 이동
+  </button>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
